2539c9ef219c167ed4421bf8169177b6766d0313
Rajan Poudel <raajan.works@gmail.com>
2025-06-11 12:37:25 +0545

    Add files via upload

---
+++ Packages/My Own Package/Main.java
@@ -0,0 +1,9 @@
+package greeting;
+import greeting.MyPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        MyPackage msg = new MyPackage();
+        msg.showMessage();
+    }
+}
---
+++ Packages/My Own Package/MyPackage.java
@@ -0,0 +1,8 @@
+package greeting;
+
+public class MyPackage {
+    public void showMessage(){
+        System.out.println("Hello From MyPackage");
+        System.out.println("Hello, I Am Rajan Poudel. I Am 20 Years Old");
+    }
+}
