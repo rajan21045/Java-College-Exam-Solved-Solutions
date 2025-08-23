ab3fbafc4aaef130cefc835307f1d41b1e24d917
Rajan Poudel <raajan.works@gmail.com>
2025-08-23 20:17:18 +0545

    Update trycatchExample.java

--- trycatchExample.java
+++ trycatchExample.java
@@ -2,7 +2,7 @@ public class trycatchExample {
 
     public static void main(String[] args) {
                 try{
-                    int a = 10/0;
+                        int a = 10/0;
                 }catch (ArithmeticException e){
                     System.out.println("Cannot Divide By Zero");
                 }
